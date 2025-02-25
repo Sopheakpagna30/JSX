9b8b42c15c44ab9141eb448b4e116ca4f69bec66
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2025-02-25 14:53:00 +0700

    Add files via upload

---
+++ Exercise2/exercise2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+function App(){
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    <p>Hello</p>
+    </>
+  );
+}
+export default App;
